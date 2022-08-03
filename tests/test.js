a8a3fea902fb9b11e1097dcf785766f23bf5975f
maksym <maxnode@swipex.com>
2022-08-03 14:52:44 +0300

    fix5

--- tests/test.js
+++ tests/test.js
@@ -1,16 +1,8 @@
-// importing express framework
-const express = require("express");
+const app = require("../app");
+const request = require('supertest');
 
-const app = express();
-
-// Respond with "hello world" for requests that hit our root "/"
-app.get("/", function (req, res) {
-  return res.send("Hello World");
+describe("GET /", () => {
+  it("respond with Hello World", (done) => {
+    request(app).get("/").expect("Hello World", done);
+  })
 });
-
-// listen to port 7000 by default
-app.listen(process.env.PORT || 7000, () => {
-  console.log("Server is running");
-});
-
-module.exports = app;
