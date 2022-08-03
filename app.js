34ef8ea831b8e33f4b5a3f932c3384ad4fa1d49f
maksym <maxnode@swipex.com>
2022-08-03 15:12:34 +0300

    fix7

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const request = require("supertest");
 // importing express framework
 const express = require("express");
 
-const index = express();
+const app = express();
 
 // Respond with "hello world" for requests that hit our root "/"
-index.get("/", function (req, res) {
+app.get("/", function (req, res) {
   return res.send("Hello World");
 });
 
 // listen to port 7000 by default
-index.listen(process.env.PORT || 7000, () => {
+app.listen(process.env.PORT || 7000, () => {
   console.log("Server is running");
 });
 
-module.exports = index;
+module.exports = app;
